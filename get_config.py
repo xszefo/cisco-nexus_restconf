89f73c418f6bfab629feb9411c0432671c1059cb
root <root@ip-178-216-201-176.e24cloud.com>
2020-03-02 17:29:12 +0100

    First commit

---
+++ get_config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+
+def main():
+    # CONSTANTS
+    host = 'sbx-nxos-mgmt.cisco.com'
+    netconf_port = '443'
+    username = 'admin'
+    password = 'Admin_1234!'
+
+
+
+
+if __name__ == '__main__':
+    main()
