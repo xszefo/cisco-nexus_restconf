9f7e98f5f5de1dd3d9c97387cf0294c98e8880eb
xszefo <root@ip-178-216-201-176.e24cloud.com>
2020-03-06 10:38:48 +0100

    testy

---
+++ get_modules.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import requests
+
+# disable warnings from SSL/TLS certificates
+requests.packages.urllib3.disable_warnings()
+
+def main():
+    # CONSTANTS
+    host = 'sbx-nxos-mgmt.cisco.com'
+    restconf_port = '443'
+    username = 'admin'
+    password = 'Admin_1234!'
+
+    headers = { 'Content-Type': 'application/yang.data+json',
+                'Accept': 'application/yang.data+json'}
+
+    url = 'https://{}/restconf/data/ietf-restconf-monitoring:restconf-state/capabilities'.format(host)
+    
+    print(url)
+
+    response = requests.get(url, auth=(username, password), headers=headers, verify=False)
+
+    print(response.text)
+    
+if __name__ == '__main__':
+    main()
