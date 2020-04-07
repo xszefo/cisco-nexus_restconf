4f2c7d5fb379ce39a5401765839884d42cb31905
xszefo <piotr.sumelka@gmail.com>
2020-04-07 11:50:06 +0200

    getting MAC table

--- get_mac_table.py
+++ get_mac_table.py
@@ -15,12 +15,17 @@ def main():
     headers = { 'Content-Type': 'application/yang.data+json',
                 'Accept': 'application/yang.data+json'}
 
-    url = 'https://{}/restconf/data/openconfig-vlan:vlans/'.format(host)
+
+    url = 'https://{}/restconf/data/Cisco-NX-OS-device:System/mac-items'.format(host) 
+    
     print(url)
 
     response = requests.get(url, auth=(username, password), headers=headers, verify=False)
 
-    print(response.text)
-    
+    response_json = response.json()['mac-items']['table-items']['vlan-items']['MacAddressEntry-list']
+
+    for line in response_json:
+        print('MAC: {} on VLAN {}'.format(line['macAddress'], line['vlan']))
+
 if __name__ == '__main__':
     main()
